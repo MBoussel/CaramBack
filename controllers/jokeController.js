c8e9e5917ced8c408bdca51abad66ce6af122551
MBoussel <mb.boussel@gmail.com>
2025-07-21 00:10:03 +0200

    feat: create route get by id

--- controllers/jokeController.js
+++ controllers/jokeController.js
@@ -10,10 +10,22 @@ export const createJoke = async (req, res) => {
         res.status(400).json({err: "Erreur lors de l'ajout de la blague"})
     }
 };
-export const getAllJokes = async (req, res) => {
+export const readAllJokes = async (req, res) => {
   try {
     const jokes = await Joke.findAll();
     res.status(200).json(jokes);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+export const readJokeById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const JokeById = await Joke.findByPk(id);
+        if (!JokeById) {
+      return res.status(404).json({ message: "Blague non trouvée" });
+    }
+    res.status(200).json(JokeById);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
--- router.js
+++ router.js
@@ -1,12 +1,14 @@
 import express from "express";
 import{
     createJoke,
-    getAllJokes
+    readAllJokes,
+    readJokeById
 } from "./controllers/jokeController.js"
 
 const router =express.Router();
 
 router.post("/api/blagues",createJoke);
-router.get("/api/blagues",getAllJokes)
+router.get("/api/blagues",readAllJokes)
+router.get("/api/blagues/:id",readJokeById)
 
 export default router
