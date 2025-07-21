735d748d1193ac441c4db116a21c14e2c8339c7c
MBoussel <mb.boussel@gmail.com>
2025-07-21 23:01:03 +0200

    fix: forget cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import router from "./router.js";
+import cors from "cors"
 import { swaggerUi, swaggerSpec } from './swagger.js';
 
 
 const app = express();
+app.use(cors());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(express.json());
